7868fd1be11c16bf4849cabebd09bc4ca1ff20b0
Datta <2096688@cognizant.com>
2022-04-30 22:48:48 +0530

    added useState on form elements

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,14 +1,28 @@
+import { useState } from "react"
 
 const AddTask = () => {
+    const [text,setText] = useState('')
+    const [reminder,setReminder] = useState(false)
   return (
     <form className="add-form">
         <div className="form-control">
             <label>Task</label>
-            <input type='text' placeholder='Add Task'></input>
+            <input 
+                type='text' 
+                placeholder='Add Task' 
+                value={text} 
+                onChange={(e)=>{
+                    setText(e.target.value)
+                }}></input>
         </div>
         <div className="form-control form-control-check">
             <label>Set Reminder</label>
-            <input type='checkbox'></input>
+            <input 
+                type='checkbox'
+                value={reminder} 
+                onChange={(e)=>{
+                    setReminder(e.currentTarget.checked)
+                }}></input>
         </div>
         <button className="btn btn-block" type="submit">Save Task</button>
     </form>
