2338922d3cce52aaf63c04a11566d47bfcabbaa9
Datta <2096688@cognizant.com>
2022-04-30 19:48:04 +0530

    added empty array message

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   return (
     <div className='container'>
         <Header/>
-        <Tasks tasks= {tasks} onDelete={deleteTask}/>
+        {tasks.length> 0 ? <Tasks tasks= {tasks} onDelete={deleteTask}/>: 'No Tasks To Show'}
     </div>
   );
 }
