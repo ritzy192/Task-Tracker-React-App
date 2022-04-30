5a365cd6add66309ac84ccdeabe1edacf94138d7
Datta <2096688@cognizant.com>
2022-04-30 18:04:31 +0530

    moved task list to App.js useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import Header from "./components/Header";
 import Tasks from './components/Tasks'
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+        
+    {
+        id:'1',
+        title:'Task 1',
+        description: 'This is task 1',
+        completed: 'false'
+    },
+    {
+        id:'2',
+        title:'Task 2',
+        description: 'This is task 2',
+        completed: 'true'
+    },
+    {
+        id:'3',
+        title:'Task 3',
+        description: 'This is task 3',
+        completed: 'false'
+    },
+    {
+        id:'4',
+        title:'Task 4',
+        description: 'This is task 4',
+        completed: 'true'
+    }])
   return (
     <div className='container'>
         <Header/>
-        <Tasks/>
+        <Tasks tasks= {tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,36 +1,11 @@
-const tasks = [{
-    id:'1',
-    title:'Task 1',
-    description: 'This is task 1',
-    completed: 'false'
-},
-{
-    id:'2',
-    title:'Task 2',
-    description: 'This is task 2',
-    completed: 'true'
-},
-{
-    id:'3',
-    title:'Task 3',
-    description: 'This is task 3',
-    completed: 'false'
-},
-{
-    id:'4',
-    title:'Task 4',
-    description: 'This is task 4',
-    completed: 'true'
-}]
-
-export const Tasks = () => {
-  return (
-      <>
-        {tasks.map((task)=>{
-            return <h3 key={task.id}>{task.title}</h3>
-        })}
-      </>
-  )
+export const Tasks = ({tasks}) => {
+    return (
+        <>
+            {tasks.map((task)=>{
+                return <h3 key={task.id}>{task.title}</h3>
+            })}
+        </>
+    )
 }
 
 export default Tasks
