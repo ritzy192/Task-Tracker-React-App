e14b55c54ec4d4ab3cc3f227ae3d6cd3fd4935cd
Datta <2096688@cognizant.com>
2022-05-02 19:26:53 +0530

    implemented Add Task API

--- src/App.js
+++ src/App.js
@@ -37,11 +37,16 @@ function App() {
       )
     }
     //Add Task
-    const addTask = (task) =>{
-      console.log(task)
-      const id = Math.floor(Math.random() * 10000) + 1
-      const newTask = {id,...task}
-      setTasks([...tasks,newTask])
+    const addTask = async (task) =>{
+      const res = await fetch(`http://localhost:5000/tasks`,{
+        method: 'POST',
+        headers:{
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+      const data = await res.json()
+      setTasks([...tasks, data])
     }
     //Delete All Tasks
     const deleteAllTasks = () =>{
